9a3f8126022f9c0d35a8246b3708c57355dbd5ea
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-13 18:28:25 +0100

    why was while?

--- tests/post.py
+++ tests/post.py
@@ -6,12 +6,11 @@ import json
 body = {'image_path': 'https://i.kym-cdn.com/photos/images/newsfeed/001/505/718/136.jpg'}
 
 
-while True:
-    myurl = "http://localhost:8888/api"
-    req = urllib.request.Request(myurl)
-    req.add_header('Content-Type', 'application/json; charset=utf-8')
-    jsondata = json.dumps(body)
-    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-    req.add_header('Content-Length', len(jsondataasbytes))
-    print (jsondataasbytes)
-    response = urllib.request.urlopen(req, jsondataasbytes)
+myurl = "http://localhost:8888/api"
+req = urllib.request.Request(myurl)
+req.add_header('Content-Type', 'application/json; charset=utf-8')
+jsondata = json.dumps(body)
+jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+req.add_header('Content-Length', len(jsondataasbytes))
+print (jsondataasbytes)
+response = urllib.request.urlopen(req, jsondataasbytes)
