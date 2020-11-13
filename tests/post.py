25b4cbeec0e6bcca3f8c7ced9c274c2ee2dd0516
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-13 18:23:35 +0100

    Tests for post update

--- tests/post.py
+++ tests/post.py
@@ -3,11 +3,11 @@
 import urllib.request
 import json
 
-body = {'image_path': 'https://cnet3.cbsistatic.com/img/hOQak1FRJY83unumvejS8f0H4ao=/1092x0/2019/10/15/0a8cb0d7-234a-4443-82e0-a17b149d6406/self-cleaning-water-bottles.jpg'}
+body = {'image_path': 'https://i.kym-cdn.com/photos/images/newsfeed/001/505/718/136.jpg'}
 
 
 while True:
-    myurl = "http://localhost:5000/api"
+    myurl = "http://localhost:8888/api"
     req = urllib.request.Request(myurl)
     req.add_header('Content-Type', 'application/json; charset=utf-8')
     jsondata = json.dumps(body)
