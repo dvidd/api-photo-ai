5d9be962647a4965d2268d1048ba8b3fddcbbf82
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-13 18:05:20 +0100

    tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,8 @@ import urllib.request
 import json
 
 while True:
-    url = "http://localhost:5000/api"
+    image = "https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2020/01/29/meme-de-una-mujer-gritando-me-dijiste-que-a-un-gato.jpeg"
+    url = f"http://localhost:5000/api?url={image}"
     req = urllib.request.Request(url)
     req.add_header('Content-Type', 'application/json; charset=utf-8')
     jsondata = json.dumps(body)
@@ -12,3 +13,5 @@ while True:
     req.add_header('Content-Length', len(jsondataasbytes))
     print (jsondataasbytes)
     response = urllib.request.urlopen(req, jsondataasbytes)
+    if jsondataasbytes != None:
+        print("No data recive")
